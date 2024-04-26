550ed765045ee8b5ae14ed1b3fd290fbf95cf1fa
Pushakr <pushkarsharma9131@gmail.com>
2024-04-27 00:35:32 +0530

    update_1

--- Server/config/razorpay.js
+++ Server/config/razorpay.js
@@ -1,6 +1,6 @@
 const Razorpay = require("razorpay");
 
 exports.instance = new Razorpay({
-  Key_id: process.env.RAZORPAY_KEY,
+  key_id: process.env.RAZORPAY_KEY,
   key_secret: process.env.RAZORPAY_SECRET,
 });
--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,61 @@
 // * big things are happing in few days
+const express = require("express");
+const db = require("./config/database");
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const cloudinary = require("./config/cloudinary");
+const fileUpload = require('express-fileupload');
+require("dotenv").config();
+
+
+const app = express();
+// port find out
+const PORT = process.env.PORT || 5000;
+
+//Database Connection
+db.connect();
+
+//middleswares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin:"http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(
+  fileUpload({
+    useTempfiles: true,
+    tempFileDir: "/tmp/",
+  })
+)
+
+//connect with cloude
+cloudinary.cloudinaryConnect();
+
+// api route mount
+const courseRoutes = require("./routes/Course");
+const paymentsRoutes = require("./routes/Payments");
+const profileRoutes = require("./routes/Profile");
+const userRoutes = require("./routes/User");
+
+app.use("/api/v1/auth", userRoutes)
+app.use("/api/v1/profile", profileRoutes)
+app.use("/api/v1/course", courseRoutes)
+app.use("/api/v1/payment", paymentsRoutes)
+
+
+//activate server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+
+//! default route
+app.get("/", () => {
+    return res.json({
+      success: true,
+      message: "Welcome to Vector Study Platform",
+    })
+})
