c6935dc73d7138028091f97c14d59d04770949fa
Pushakr <pushkarsharma9131@gmail.com>
2024-10-26 21:43:39 +0530

    change

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -9,7 +9,7 @@ import { NavbarLinks } from "../../data/navbar-links"
 import { apiConnector } from "../../services/apiconnector"
 import { categories } from "../../services/apis"
 import { ACCOUNT_TYPE } from "../../utils/constants"
-import ProfileDropdown from "../core/Auth/ProfileDrop"
+import ProfileDropdown from "../core/Auth/ProfileDropdown"
 
 // const subLinks = [
 //   {
