8a107c249818707e9d894de7e5bb25aa391387ba
Pushakr <pushkarsharma9131@gmail.com>
2024-10-26 21:55:58 +0530

    more_add_file

--- src/components/core/Dashboard/Settings/UpdatePassword.jsx
+++ src/components/core/Dashboard/Settings/UpdatePassword.jsx
@@ -4,7 +4,7 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
-import { changePassword } from "../../../../services/operations/SettingsAPI"
+import { changePassword } from "../../../../services/operations/settingsAPI"
 import IconBtn from "../../../common/IconBtn"
 
 export default function UpdatePassword() {
